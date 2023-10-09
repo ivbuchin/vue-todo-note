bcfc789caab9df2cf03b1dc030a5de1780650ff3
VanLye <ivbuchin@gmail.com>
2023-10-09 19:26:18 +0500

    Del Avatar Red main.js Clean serializers.py

--- TodoNote/users/models.py
+++ TodoNote/users/models.py
@@ -4,4 +4,4 @@ from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
-    avatar = models.ImageField("Avatar image", upload_to='users', null=True, blank=True)
+    pass
